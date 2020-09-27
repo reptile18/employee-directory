6c99ebf355c7f8fd473a368cb220f35b658bafcd
Johnny Li <reptile18@gmail.com>
2020-09-27 10:08:05 -0700

    add debug for process env

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import EmployeeDirectory from './components/EmployeeDirectory';
 
 function App() {
+  useEffect(() => {
+    console.log({process.env});
+  });
   return (
     <div className="App">
       <EmployeeDirectory />
